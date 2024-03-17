5be8e7a8ab53f1ae0a3091e04f713820f9e6e082
Felix <fedecraft3312@gmail.com>
2024-03-17 14:37:14 -0300

    Added server configuration router default export

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import SERVER_URL_MAPPINGS from "./env/SERVER_URL_MAPPINGS"
 import { expressAuthenticationUrl, backdoorServerAccessUrl, realEstateUrl } from "./server";
 import LocationSelection from "./server/LocationSelection";
 import AppServer from "./server/AppServer";
+import serverConfigurationRouter from "./routes";
 
 export default {
     SERVER_URL_MAPPINGS,
+    
+    serverConfigurationRouter,
     
     AppServer,
     LocationSelection,
