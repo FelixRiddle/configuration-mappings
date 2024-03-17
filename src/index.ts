f38691d2a0ebd3431feb1fdf1b385e1178184975
Felix <fedecraft3312@gmail.com>
2024-03-17 14:17:54 -0300

    Added default exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import SERVER_URL_MAPPINGS from "./env/SERVER_URL_MAPPINGS"
 
+import { expressAuthenticationUrl, backdoorServerAccessUrl, realEstateUrl } from "./server";
+import LocationSelection from "./server/LocationSelection";
+import AppServer from "./server/AppServer";
+
 export default {
     SERVER_URL_MAPPINGS,
+    
+    AppServer,
+    LocationSelection,
+    
+    expressAuthenticationUrl,
+    backdoorServerAccessUrl,
+    realEstateUrl,
 };
