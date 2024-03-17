94492e0506589d16e6ec8067e259a820110500a3
Felix <fedecraft3312@gmail.com>
2024-03-17 15:18:41 -0300

    Forgot to include server config route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 // Servers must implement this route at the root part
 import express from "express";
 
+import serverConfigRouter from "./server_config";
+
 const serverConfigurationRouter = express.Router();
 
-serverConfigurationRouter.use("/server_config", )
+serverConfigurationRouter.use("/server_config", serverConfigRouter);
 
 export default serverConfigurationRouter;
