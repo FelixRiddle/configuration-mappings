40c9228be7b947d9b02f3588c183804438d22589
Felix <fedecraft3312@gmail.com>
2024-03-17 13:10:26 -0300

    Test server exists

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,10 @@
+import { testServerExists } from "./server/AppServer.test";
 
 /**
  * Test all
  */
 export default async function testAll() {
     console.log(`Running all tests`);
+    
+    testServerExists();
 }
