3fc2892046c35d110523dc54e86e88ff1c5871f5
Felix <fedecraft3312@gmail.com>
2024-03-17 13:49:22 -0300

    Update location urls

--- src/server/LocationSelection.ts
+++ src/server/LocationSelection.ts
@@ -1,3 +1,5 @@
+import AppServerType from "../types/server/AppServerType";
+import AppServer from "./AppServer";
 
 /**
  * Location selection
@@ -5,5 +7,16 @@
 export default class LocationSelection {
     constructor() { }
     
-    
+    /**
+     * Update location urls to those that are on the app server file
+     */
+    updateLocationUrls() {
+        const appServer: AppServer = new AppServer();
+        
+        const servers: AppServerType = appServer.servers();
+        
+        process.env["AUTHENTICATION_URL"] = servers["express-authentication"];
+        process.env["BACKDOOR_SERVER_ACCESS_URL"] = servers["backdoor-server-access"];
+        process.env["REAL_ESTATE_URL"] = servers["express-real-estate"];
+    }
 }
--- src/types/server/AppServerType.ts
+++ src/types/server/AppServerType.ts
@@ -6,6 +6,7 @@ export default interface AppServerType {
     "good-roots-pug-view"?: string,
     "general-frontend"?: string,
     "express-authentication"?: string,
+    "backdoor-server-access"?: string,
     "express-real-estate"?: string,
 };
 
