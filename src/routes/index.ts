3417c2a1a8172dccd0238156c931ac214fcc2a8e
Felix <fedecraft3312@gmail.com>
2024-03-17 14:33:50 -0300

    Update environment variables

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+// Servers must implement this route at the root part
+import express from "express";
+
+const serverConfigurationRouter = express.Router();
+
+serverConfigurationRouter.use("/server_config", )
+
+export default serverConfigurationRouter;
