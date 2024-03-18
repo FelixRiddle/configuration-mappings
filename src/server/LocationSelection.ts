5000ac4a79b98f3fe86c67eab2aaf1b7d37cf9ce
Felix <fedecraft3312@gmail.com>
2024-03-18 12:36:28 -0300

    Added a patch version

--- src/server/LocationSelection.ts
+++ src/server/LocationSelection.ts
@@ -1,6 +1,7 @@
-import { BACKDOOR_SERVER_ACCESS_KEYWORD, EXPRESS_AUTHENTICATION_KEYWORD, REAL_ESTATE_KEYWORD } from "./index";
+import { BACKDOOR_SERVER_ACCESS_KEYWORD, EXPRESS_AUTHENTICATION_KEYWORD, REAL_ESTATE_KEYWORD, backdoorServerAccessUrl, expressAuthenticationUrl, realEstateUrl } from "./index";
 import AppServerType from "../types/server/AppServerType";
 import AppServer from "./AppServer";
+import { SERVERS_DEFAULT_LOCATION } from "../env/SERVER_URL_MAPPINGS";
 
 /**
  * Location selection
@@ -19,5 +20,42 @@ export default class LocationSelection {
         process.env[EXPRESS_AUTHENTICATION_KEYWORD] = servers["express-authentication"];
         process.env[BACKDOOR_SERVER_ACCESS_KEYWORD] = servers["backdoor-server-access"];
         process.env[REAL_ESTATE_KEYWORD] = servers["express-real-estate"];
+    }
+    
+    // --- Location selection ---
+    /**
+     * Express authentication or default
+     */
+    static expressAuthentication() {
+        const url = expressAuthenticationUrl();
+        if(!url) {
+            return SERVERS_DEFAULT_LOCATION["express-authentication"];
+        }
+        
+        return url;
+    }
+    
+    /**
+     * 
+     */
+    static backdoorServerAccess() {
+        const url = backdoorServerAccessUrl();
+        if(!url) {
+            return SERVERS_DEFAULT_LOCATION["backdoor-server-access"];
+        }
+        
+        return url;
+    }
+    
+    /**
+     * 
+     */
+    static realEstate() {
+        const url = realEstateUrl();
+        if(!url) {
+            return SERVERS_DEFAULT_LOCATION["express-real-estate"];
+        }
+        
+        return url;
     }
 }
