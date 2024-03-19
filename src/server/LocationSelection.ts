005d615865c0b5aba1f8d41a66e8cf8c000c5f0c
Felix <fedecraft3312@gmail.com>
2024-03-19 13:17:39 -0300

    Implemented server start with port seeking

--- src/server/LocationSelection.ts
+++ src/server/LocationSelection.ts
@@ -1,3 +1,7 @@
+import { Express } from "express";
+
+import PortSeeker from "felixriddle.port-seeker";
+
 import { BACKDOOR_SERVER_ACCESS_KEYWORD, EXPRESS_AUTHENTICATION_KEYWORD, REAL_ESTATE_KEYWORD, backdoorServerAccessUrl, expressAuthenticationUrl, realEstateUrl } from "./index";
 import AppServerType from "../types/server/AppServerType";
 import AppServer from "./AppServer";
@@ -5,6 +9,26 @@ import { SERVERS_DEFAULT_LOCATION } from "../env/SERVER_URL_MAPPINGS";
 
 /**
  * Location selection
+ * 
+ * Features:
+ * 
+ * - Update location urls
+ * This update project's local environment variables to that of the file app_server.json
+ * 
+ * - Server urls
+ * These functions have the name of the repositories they fetch the url from the environment
+ * or use the default if it fails.
+ * 
+ * - Location selector(selectLocation)
+ * You give an express instance to this function and it automatically makes three attempts
+ * to start a server at a given port:
+ * 1) Use env.SERVER_PORT
+ * 2) Use the default server port
+ * 3) Use an ephemeral port
+ * This third step does extra things like:
+ * * Updates app_server.json
+ * * Sends discovery request to every server, this tells other servers that the url of
+ * a server has changed.
  */
 export default class LocationSelection {
     constructor() { }
@@ -20,6 +44,104 @@ export default class LocationSelection {
         process.env[EXPRESS_AUTHENTICATION_KEYWORD] = servers["express-authentication"];
         process.env[BACKDOOR_SERVER_ACCESS_KEYWORD] = servers["backdoor-server-access"];
         process.env[REAL_ESTATE_KEYWORD] = servers["express-real-estate"];
+    }
+    
+    /**
+     * Select a location for a given express app
+     * 
+     * Port priority
+     * process.env.SERVER_PORT > Default url port > Random ephemeral port
+     * 
+     */
+    async selectLocation(app: Express) {
+        
+        // Attempt 3
+        const attempt3 = async () => {
+            // --- Ephemeral port ---
+            console.log(`Attempt 3`);
+            
+            // The original port didn't work?
+            // Let's use another one
+            const seeker = new PortSeeker();
+            const port = await seeker.firstOpen();
+            
+            // TODO: Set the new port in configuration
+            
+            // TODO: Make other servers aware of it
+            
+            // If the server couldn't start then we will try with another port
+            const serverInstance = app.listen(port, () => {
+                console.log(`Server running at http://${process.env.SERVER_HOST}:${port}`);
+            });
+            
+            serverInstance.on('error', (err) => {
+                // console.log(`On error`);
+                // console.error(err);
+                
+                console.log(`Server couldn't start!`);
+                console.log(`No more attempts`);
+            });
+        }
+        
+        const attempt2 = async () => {
+            // --- Default port ---
+            console.log(`Attempt 2`);
+            
+            // Parse url
+            const defaultUrl = SERVERS_DEFAULT_LOCATION['express-authentication'];
+            const parsedDefaultUrl = new URL(defaultUrl);
+            
+            const port = parsedDefaultUrl.port;
+            
+            console.log(`New port: `, port);
+            
+            const serverInstance = app.listen(port, () => {
+                console.log(`Server running at http://${process.env.SERVER_HOST}:${port}`);
+            });
+            
+            serverInstance.on('error', async (err) => {
+                // console.log(`On error`);
+                // console.error(err);
+                
+                console.log(`Server couldn't start!`);
+                await attempt3();
+            });
+        }
+        
+        const attempt1 = async () => {
+            // --- Environment port ---
+            // Will use 3000 if available, otherwise fall back to a random port
+            // Try to open the server
+            let unparsedPort = process.env.SERVER_PORT;
+            
+            console.log(`Attempt 1`);
+            
+            // If there's no port go to the next attempt
+            if(typeof unparsedPort === "undefined") {
+                return await attempt2();
+            }
+            
+            // Parse port
+            let port: number = parseInt(unparsedPort);
+            
+            console.log(`Port: `, port);
+            
+            const instance = app.listen(port, () => {
+                console.log(`Server running at http://${process.env.SERVER_HOST}:${port}`);
+            });
+            
+            // To catch errors you have to do this
+            // (I didn't know 😭😭)
+            return instance.on('error', async (err) => {
+                // console.log(`On error`);
+                // console.error(err);
+                
+                console.log(`Server couldn't start!`);
+                return await attempt2();
+            });
+        }
+        
+        await attempt1();
     }
     
     // --- Location selection ---
